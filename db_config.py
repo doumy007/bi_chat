b3340ad6ef568a1051f4af3f71e926728f21ead3
doumy007 <42424466+doumy007@users.noreply.github.com>
2026-03-09 15:13:12 -0300

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,3 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
 # Configuración de la base de datos MySQL
 DB_CONFIG = {
     "host": "50.6.61.23",
@@ -5,6 +11,3 @@ DB_CONFIG = {
     "password": "nufjztte_googleads",
     "database": "nufjztte_googleads"
 }
-
-# Configuración de OpenAI (Asegúrate de poner tu API Key aquí)
-OPENAI_API_KEY = "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"
