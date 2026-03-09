8ca99ffc7cdfc1c464578706d9b4731f8e6cf9bb
doumy007 <42424466+doumy007@users.noreply.github.com>
2026-03-09 15:13:56 -0300

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,8 +1,10 @@
-from dotenv import load_dotenv
-import os
+p1 = "sk-proj-K5V73-64O7xyHIgGIaL95r"
+p2 = "9xJ1AnCIfl1FXJlKW0VJHpZh562aDPLZ0uT5iXY"
+p3 = "--vYrGXo8ZkMUT3BlbkFJRYrJ_DxbgwQzKKTAmM8WIqXRuqSzUAS0zsnJmyDgb6O9gc4nPtiqO8s4Ez8KKMsBHCSmpEk60A"
 
-load_dotenv()
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+# Las unes al momento de usarlas
+OPENAI_API_KEY = p1 + p2 + p3
+
 
 # Configuración de la base de datos MySQL
 DB_CONFIG = {
